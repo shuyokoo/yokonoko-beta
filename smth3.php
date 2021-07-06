bddacecad66fea3df22f8e38780a66b996303c0c
shuyokoo <86910670+shuyokoo@users.noreply.github.com>
2021-07-06 00:20:58 -0700

    Create smth3.php

---
+++ smth3.php
@@ -0,0 +1,12 @@
+	
+<p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+    //If user wants to end session
+    $("#exit").click(function(){
+        var exit = confirm("Are you sure you want to end the session?");
+        if(exit==true){window.location = 'index.php?logout=true';}      
+    });
+});
+</script>
